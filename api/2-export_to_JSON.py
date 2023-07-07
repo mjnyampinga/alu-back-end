7968aa9f8e9dd37b62e2b28499f0ee2520c7a7d6
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-07-07 12:54:42 +0000

    API

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-""""Module"""
+"""gathers data from an API"""
 
 import json
 import requests
 import sys
 
-if __name__ == '__main__':
+"""a Python script to export data in the JSON format"""
+
+if __name__ == "__main__":
     employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
 
-    user_info = requests.request('GET', user_url).json()
-    todos_info = requests.request('GET', todos_url).json()
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
 
-    employee_username = user_info["username"]
+    employee_username = user_info.get("username")
 
     todos_info_sorted = [
         dict(zip(["task", "completed", "username"],
                  [task["title"], task["completed"], employee_username]))
-        for task in todos_info]
+        for task in todo_info]
 
     user_dict = {str(employee_id): todos_info_sorted}
-    with open(str(employee_id) + '.json', "w") as file:
-        file.write(json.dumps(user_dict))
+    with open(str(employee_id) + '.json', "w") as f:
+        f.write(json.dumps(user_dict))

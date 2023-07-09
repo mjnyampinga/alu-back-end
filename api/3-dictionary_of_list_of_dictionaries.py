1a7076b773998d7228b0e2613a27e7e23ffa7445
Marie Joselyne Nyampinga <122269880+mjnyampinga@users.noreply.github.com>
2023-07-09 10:01:35 +0000

    API

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-    python script that exports data in the JSON format
-"""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-    """
-        export to JSON
-    """
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
